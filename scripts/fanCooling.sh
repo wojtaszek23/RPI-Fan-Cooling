603099deae546449908da33214fa394b4bde7185
wojtaszek23 <wojtaszek23@wp.pl>
2020-01-03 11:32:00 +0100

    Script is working. Small refactors left to be done

--- scripts/fanCooling.sh
+++ scripts/fanCooling.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-readonly PIN=21
-readonly TRESHOLD_OFF=38
-readonly TRESHOLD_ON=58
-readonly SLEEP=10
+PIN=21
+TRESHOLD_OFF=38
+TRESHOLD_ON=58
+SLEEP=10
 cooling_state=0
 
 function get_temperature() {
@@ -11,15 +11,18 @@ function get_temperature() {
   assign_char="="
   temperature_string=${temperature_string#*"="}
   temperature_string=${temperature_string%%"."*}
-  return $(temperature_string + 0)
+  #echo current temperature: $temperature_string
+  echo $(( $temperature_string + 0 ))
 }
 
 function init_pin() {
   pin=$1
   dir=$2
   pin_name="gpio$pin"
-  echo $pin > /sys/class/gpio/export
-  echo $dir > /sys/class/gpio/$(pin_name)/direction
+  #echo $pin_name
+  echo "$pin" > /sys/class/gpio/export
+  sleep 1
+  echo $dir > /sys/class/gpio/$pin_name/direction
 }
 
 function terminate_pin() {
@@ -31,25 +34,37 @@ function set_pin_value() {
   pin=$1
   value=$2
   pin_name="gpio$pin"
-  echo $value > /sys/class/gpio/$(pin_name)/value 
+  echo $value > /sys/class/gpio/$pin_name/value 
 }
 
 function main_loop () {
-  x=1
-  while [ $x -le 100 ]
+  current_state="not cooling"
+  init_pin $PIN out
+  
+  while [ 1 ]
   do
+    case $current_state in
+      "not cooling")
+        current_temperature=$(get_temperature)
+        if [ $current_temperature -ge $TRESHOLD_ON ] ; then
+          set_pin_value $PIN 1
+          current_state="cooling"
+          echo start cooling
+        fi
+        echo not cooling
+        ;;
+      "cooling")
+        current_temperature=$(get_temperature)
+        if [ $current_temperature -le $TRESHOLD_OFF ] ; then
+          set_pin_value $PIN 0
+          current_state="not cooling"
+          echo stop cooling
+        fi
+        echo cooling
+        ;;
+    esac
 
-#    if [ $(( $x % 2 )) -ne 0 ] ; then
-#      echo 0
-#    else
-#      echo 1
-#    fi
-
-    echo $(( $x % 2 ))
-    
-    x=$(( $x + 1 ))
-    
-    sleep 1
+    sleep 10
   done
 }
 
